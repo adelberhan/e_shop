a121f35a20c07537da0d5cd5400e76483eb8ed6c
Adel Berhan <adel.berhan@gmail.com>
2024-01-15 07:34:04 +0200

    update JWT

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -10,8 +10,8 @@ function authJwt() {
     isRevoked: isRevoked,
   }).unless({
     path: [
-      { url: /\/public\/uploads(.*)/, methods: ['GET','PUT', 'OPTIONS'] },
-            { url: /\/api\/v1\/products(.*)/, methods: ['GET','PUT', 'OPTIONS'] },
+      { url: /\/public\/uploads(.*)/, methods: ['GET','PUT','POST', 'OPTIONS'] },
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET','POST','PUT', 'OPTIONS'] },
             { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
             { url: /\/api\/v1\/users(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
